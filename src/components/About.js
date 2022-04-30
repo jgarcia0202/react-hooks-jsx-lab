6feb29e4617a9da029a102a4f27ab427d04be9f6
Jose <jmgarcia0202@gmail.com>
2022-04-30 02:35:50 -0700

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { image } from "../data/data";
+import {image} from "../data/data.js";
 
 function About() {
-  return <div>About</div>;
+  // return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Hello I'm Jose from California, I am attending Flatiron to become a software engineer.</p>
+      <img src = {image} alt="i made this" />
+    </div>
+  );
 }
 
 export default About;
